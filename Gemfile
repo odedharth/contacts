599e5e25d1e0a9075e9075fbac2bc2b4b16da6f4
Oded Harth <odedharth@gmail.com>
2012-12-04 23:15:48 +0200

    changed index

--- Gemfile
+++ Gemfile
@@ -24,6 +24,9 @@ end
 
 gem 'jquery-rails'
 
+gem 'soulmate'
+gem 'faker'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Contacts::Application.routes.draw do
   resources :contacts
 
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +52,7 @@ Contacts::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'contacts#index'
 
   # See how all your routes lay out with "rake routes"
 
